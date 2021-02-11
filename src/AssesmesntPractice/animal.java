78f46fcbdb2165fe31935855e7272742090e012e
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-02-11 11:29:02 -0600

    practice for java assessment

--- src/AssesmesntPractice/animal.java
+++ src/AssesmesntPractice/animal.java
@@ -1,2 +1,25 @@
-package AssesmesntPractice;public class animal {
+package AssesmesntPractice;
+
+public class animal {
+    private String name;
+    private String species;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String roar(){
+        return "I am "+this.name +", hear me roar!";
+    }
 }
